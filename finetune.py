116618cbb203245cc4acbcb2561ce51aad5c09de
Aleksandr Drozd <alexander.drozd@gmail.com>
2025-07-13 13:17:18 +0900

    set up lora

--- finetune.py
+++ finetune.py
@@ -64,6 +64,8 @@ def main():
         "unsloth/gemma-3-4b-pt",
         load_in_4bit=False,  # Use 4bit to reduce memory use. False for 16bit LoRA.
         use_gradient_checkpointing="unsloth",  # True or "unsloth" for long context
+        r=16,  # Add LoRA rank
+        lora_alpha=32,  # Add LoRA alpha
     )
 
     processor = get_chat_template(processor, "gemma-3")
